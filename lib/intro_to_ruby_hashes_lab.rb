24d4fecd2902bffe148ea4b379c52abf6ead4922
Sara Bastian <seb118@georgetown.edu>
2020-09-16 17:33:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 
 def update_counting_hash(hash, key)
   if  hash[key]
-  hash[key] += key
+  hash[key] +1 
 else hash[key] = 1
   
   # given a hash an a key as parameters, return an updated hash
