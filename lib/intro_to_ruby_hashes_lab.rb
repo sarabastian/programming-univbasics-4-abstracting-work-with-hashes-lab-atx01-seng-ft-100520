e061cb591ec0b4bec6fded7b60b88d0bcd16c30a
Sara Bastian <seb118@georgetown.edu>
2020-09-16 17:48:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if  key
+  if  hash[key]
    key += 1 
 else key = 1
   # given a hash an a key as parameters, return an updated hash
